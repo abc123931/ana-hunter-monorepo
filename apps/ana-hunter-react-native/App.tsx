4274a2a0aa0b8dc6a490cf272f1a9c9c04ab06e0
abc123931 <abc123931@gmail.com>
2021-12-12 10:14:00 +0900

    ✨ Add test package.

--- apps/ana-hunter-react-native/App.tsx
+++ apps/ana-hunter-react-native/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { greeting } from "cool-package"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{greeting}</Text>
       <StatusBar style="auto" />
     </View>
   );
